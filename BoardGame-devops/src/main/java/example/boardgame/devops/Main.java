f1207beb3fe5ea81c32b05778e4174aa70136e7d
João Augusto Câmara Lopes Costa <camarajoaoaugusto@gmail.com>
2025-09-29 20:57:14 -0300

    Criando o pacote inicial do projeto

---
+++ BoardGame-devops/src/main/java/example/boardgame/devops/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package example.boardgame.devops;
+
+/**
+ *
+ * @author camar
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
